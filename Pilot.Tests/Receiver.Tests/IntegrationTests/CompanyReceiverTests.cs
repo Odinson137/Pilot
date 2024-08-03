4c32fe51b3af801d4db76537a5477db4cb17c55f
Odinson137 <buryy137@gmail.com>
2024-08-03 16:37:27 +0300

    Add test summary into in ci-cd

--- Pilot.Tests/Receiver.Tests/IntegrationTests/CompanyReceiverTests.cs
+++ Pilot.Tests/Receiver.Tests/IntegrationTests/CompanyReceiverTests.cs
@@ -1,6 +1,4 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using Pilot.Contracts.Base;
 using Pilot.Contracts.DTO.ModelDto;
 using Pilot.Contracts.Models;
