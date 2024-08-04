0c6c4bb0be096e76c2de25e365f42da9e5fecc3f
Odinson137 <buryy137@gmail.com>
2024-08-05 00:04:28 +0300

    fix ModelType name

---
+++ Pilot.Contracts/Pilot.BaseContract/Data/Enums/ModelType.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using Pilot.Contracts.DTO.ModelDto;
+
+namespace Pilot.Contracts.Data.Enums;
+
+/// <summary>
+/// Этот enum требуется, чтоб классифицировать сущности в бд. Например, в модели Message
+/// Используется Dto вместо ссылок на реальные модели, чтобы не делать ссылки из контрактов на все другие проекты
+/// </summary>
+public enum ModelType
+{
+    [Description(nameof(UserDto))]
+    User = 1,
+    
+    [Description(nameof(CompanyDto))]
+    Company = 2,
+
+    [Description(nameof(CompanyUserDto))]
+    CompanyUser = 3,
+    
+    [Description(nameof(ProjectDto))]
+    Project = 4,
+    
+    [Description(nameof(FileDto))]
+    File = 5,
+    
+    [Description(nameof(MessageDto))]
+    Message = 6,
+    
+    [Description(nameof(ProjectTaskDto))]
+    ProjectTask = 6,
+    
+    [Description(nameof(TeamDto))]
+    Team = 7,
+    
+    [Description(nameof(HistoryActionDto))]
+    HistoryAction = 8,
+    
+    [Description(nameof(MessageUserDto))]
+    MessageUser = 9,
+}
