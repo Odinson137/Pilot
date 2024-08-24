f0dafc7cfde62f0eaf616a8a258b412fccbb76fa
Odinson137 <buryy137@gmail.com>
2024-08-24 12:22:53 +0300

    refactoring

--- Pilot.Receiver/Models/Company.cs
+++ Pilot.Receiver/Models/Company.cs
@@ -7,11 +7,13 @@ namespace Pilot.Receiver.Models;
 public class Company : BaseModel, IAddCompanyUser
 {
     [Required] [MaxLength(50)] public string Title { get; init; } = null!;
+
     [MaxLength(500)] public string? Description { get; init; }
 
     public ICollection<Project> Projects { get; set; } = new List<Project>();
 
     public ICollection<Team> Teams { get; set; } = new List<Team>();
+
     public ICollection<CompanyUser> CompanyUsers { get; set; } = new List<CompanyUser>();
 
     public void AddCompanyUser(CompanyUser companyUser)
