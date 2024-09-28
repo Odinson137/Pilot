c1522448c815cbdb4eaf4f52fb856c1077118365
Odinson137 <buryy137@gmail.com>
2024-09-28 09:41:18 +0300

    add capability tests

---
+++ Pilot.Tests/Test.Capability/IntegrationTests/PostTests.cs
@@ -0,0 +1,8 @@
+﻿using Pilot.Capability.Models;
+using Pilot.Contracts.DTO.ModelDto;
+using Test.Capability.Factories;
+
+namespace Test.Capability.IntegrationTests;
+
+public class PostSkillTests(CapabilityTestCapabilityFactory factory)
+    : BaseModelReceiverIntegrationTest<Post, PostDto>(factory);
---
+++ Pilot.Tests/Test.Capability/IntegrationTests/UserSkillTests.cs
@@ -0,0 +1,8 @@
+﻿using Pilot.Capability.Models;
+using Pilot.Contracts.DTO.ModelDto;
+using Test.Capability.Factories;
+
+namespace Test.Capability.IntegrationTests;
+
+public class UserSkillTests(CapabilityTestCapabilityFactory factory)
+    : BaseModelReceiverIntegrationTest<UserSkill, UserSkillDto>(factory);
