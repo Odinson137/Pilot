a992b36b4c1befd1ae9c11354c4abd9c343886a4
Odinson137 <buryy137@gmail.com>
2024-09-29 12:46:55 +0300

    fix

--- Pilot.Tests/Test.Storage/IntegrationTests/FileTests.cs
+++ Pilot.Tests/Test.Storage/IntegrationTests/FileTests.cs
@@ -1,8 +1,9 @@
 ﻿using Pilot.Contracts.DTO.ModelDto;
 using Test.Storage.IntegrationTests.Factories;
+using Xunit.Abstractions;
 using File = Pilot.Storage.Models.File;
 
 namespace Test.Storage.IntegrationTests;
 
-public class FileTests(StorageTestStorageFactory factory)
-    : BaseModelIntegrationTest<File, FileDto>(factory);
+public class FileTests(StorageTestStorageFactory factory, ITestOutputHelper testOutputHelper)
+    : BaseModelIntegrationTest<File, FileDto>(factory, testOutputHelper);
