311c78a3714300565d3f7cdf6e7f3c16ecabc21e
Odinson137 <buryy137@gmail.com>
2025-02-01 20:27:14 +0300

    add beautiful pages for registration and authorization

--- Pilot.BlazorClient/ViewModels/UserViewModels/RegistrationUserDto.cs
+++ Pilot.BlazorClient/ViewModels/UserViewModels/RegistrationUserDto.cs
@@ -3,10 +3,13 @@
 public class RegistrationUserViewModel
 {
     public string UserName { get; set; }
-    
+
     public string Name { get; set; }
-    
+
     public string LastName { get; set; }
-    
+
     public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+    
+    public bool AgreeToTerms { get; set; }
 }
